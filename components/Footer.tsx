75a07e994f838db6df4d6894a4daea91f04e9d0a
unknown <newli5737@gmail.com>
2026-03-13 16:20:31 +0700

    new update 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -85,7 +85,7 @@ export default function Footer() {
 
           {/* Main footer grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mb-12">
-            
+
             {/* Company Info */}
             <motion.div
               initial={{ opacity: 0, y: 20 }}
